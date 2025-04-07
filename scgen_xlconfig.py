6d46402dd94a428daa437926be8f23a0361ba5c5
fnlnandy <andyfaniloniaina01@gmail.com>
2025-04-07 18:46:24 +0300

    Add Excel configs file.

---
+++ scgen_xlconfig.py
@@ -0,0 +1,12 @@
+## Voici la liste des configurations
+## du fichier Excel, plus précisément
+## le nom des headers.
+
+ID_HEADER           = "Numéro matricule"
+FULL_NAME_HEADER    = "NOM & Prénom(s)"
+DOB_AND_POB_HEADER  = "Date et Lieu de naissance"
+PHONE_NUMBER_HEADER = "Numéro téléphone"
+EMAIL_HEADER        = "E-Mail"
+ADDRESS_HEADER      = "Adresse exacte"
+NIC_NUMBER_HEADER   = "Numéro CIN"
+NIC_DAPOD_HEADER    = "Quand et où vous avez obtenu votre CIN"
